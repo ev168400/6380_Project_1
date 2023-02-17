75fb765ef425e8630bba8d50bdb993509715169f
Eddie Villarreal <ejv190000@utdallas.edu>
2023-02-17 14:38:50 -0600

    Inital TCP Client updates

---
+++ TCPClient.java
@@ -0,0 +1,46 @@
+import java.net.*;
+import java.io.*;
+
+public class TCPClient {
+
+    String ServerHost;
+    String ClientHost;
+	int ServerPort;
+    int UID;
+    int serverUID;
+	Socket ClientSocket;
+	ObjectInputStream inStream;
+	ObjectOutputStream outStream;
+	Node dsNode;
+
+    public TCPClient(int UID, int serverPort, String ServerHost, String ClientHost, int serverUID,
+			Node _dsNode) {
+		this.ServerHost = ServerHost;
+		this.ServerPort = serverPort;
+		this.UID = UID;
+		this.ClientHost = ClientHost;
+		this.serverUID = serverUID;
+		this.dsNode = _dsNode;
+	}
+
+    public void listenSocket() {
+		// Create socket connection
+		try {
+			ClientSocket = new Socket(ServerHost, ServerPort, InetAddress.getByName(ClientHost), 0);
+			outStream = new ObjectOutputStream(ClientSocket.getOutputStream());
+			outStream.flush();
+
+			inStream = new ObjectInputStream(ClientSocket.getInputStream());
+			System.out.println("After inputStream, listenSocket");
+		} catch (UnknownHostException e) {
+			System.out.println("Unknown host:" + ServerHost);
+			System.exit(1);
+		} catch (IOException e) {
+			System.out.println("No I/O" + e);
+			System.exit(1);
+		}
+	}
+    
+
+
+}
