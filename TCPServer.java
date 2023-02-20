523b8dbe11c8f14e70fe1f554a7101cdac9a4dc9
Eddie Villarreal <ejv190000@utdallas.edu>
2023-02-20 15:03:46 -0600

    Starting TCP Connections

--- TCPServer.java
+++ TCPServer.java
@@ -1,3 +1,11 @@
+import java.net.ServerSocket;
+
+//Server Class
 public class TCPServer {
+    String hostName;
+    int portNumber;
+    int UID;
+    ServerSocket serverSocket;
+    Node node;
     
 }
